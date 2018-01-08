9e696b6ec9e3f1eea39afc7442b54d6b54e5cfbe
veera83372 <veeravenky222@gmail.com>
2018-01-08 16:09:02 +0530

    Maintaing checker instead of case sensitive flag for strict parser

--- src/strictParseInfo.js
+++ src/strictParseInfo.js
@@ -3,25 +3,16 @@ const ParseInfo=require("./parseInfo.js");
 const InvalidKeyError=require("./errors/invalidKeyError.js");
 
 
-
-const contains=function(list,key,caseSensitive) {
-  return list.find(function(validKey){
-    if(caseSensitive)
-      return key==validKey;
-    return key.toLowerCase()==validKey.toLowerCase();
-  });
-}
-
-var StrictParseInfo=function(initialParsingFunction,validKeys,caseSensitive) {
+var StrictParseInfo=function(initialParsingFunction,validKeys,checker) {
   ParseInfo.call(this,initialParsingFunction);
-  this.caseSensitive=caseSensitive;
+  this.checker=checker;
   this.validKeys=validKeys;
 }
 
 StrictParseInfo.prototype=Object.create(ParseInfo.prototype);
 
 StrictParseInfo.prototype.pushKeyValuePair=function() {
-  if(!contains(this.validKeys,this.currentKey,this.caseSensitive))
+  if(!this.checker(this.validKeys,this.currentKey))
     throw new InvalidKeyError("invalid key",this.currentKey,this.currentPos);
   this.parsedKeys[this.currentKey]=this.currentValue;
   this.resetKeysAndValues();
--- src/strictParser.js
+++ src/strictParser.js
@@ -1,10 +1,22 @@
 const Parser=require("./keyValueParser.js");
 const strictParseInfoCreator=require("./parseInfoCreator.js").strict;
+const contains=function(list,key) {
+  return list.find(function(validKey){
+    return key==validKey;
+  });
+}
+
+const ignoreCaseContains = function(list,key) {
+  return list.find(function(validKey){
+    return key.toLowerCase()==validKey.toLowerCase();
+  });
+}
 
 var StrictParser=function(listOfKeys,caseSensitive=true) {
+  let checker = caseSensitive ? contains : ignoreCaseContains;
   Parser.call(this);
   let sanitisedListOfKeys=listOfKeys||[];
-  this.parseInfoCreator=strictParseInfoCreator(sanitisedListOfKeys,caseSensitive);
+  this.parseInfoCreator=strictParseInfoCreator(sanitisedListOfKeys,checker);
 }
 
 StrictParser.prototype=Object.create(Parser.prototype);

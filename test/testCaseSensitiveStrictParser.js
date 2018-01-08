c2ed7d852193e494c4bfd031cd78289cd41e3bc7
veera83372 <veeravenky222@gmail.com>
2018-01-08 16:53:02 +0530

    3 more tests added in strict parser test file

--- test/testCaseSensitiveStrictParser.js
+++ test/testCaseSensitiveStrictParser.js
@@ -13,6 +13,8 @@ describe("strict parser that is case insensitive",function(){
     let parsed=kvParser.parse("NAME=jayanth");
     assert.deepEqual(parsed,expected);
   });
+
+  
 });
 
 describe("strict parser that is case sensitive",function(){
@@ -23,4 +25,28 @@ describe("strict parser that is case sensitive",function(){
       kvParser.parse("NAME=jayanth");
     })
   });
+
+  it('should parse when specified keys are in same case as actual ',function () {
+    let kvParser=new StrictParser(["Name"]);
+    let actual = kvParser.parse("Name=veera");
+    let expected=new Parsed();
+    expected.Name="veera";
+    assert.deepEqual(actual,expected);
+  })
+
+  it('should parse when specified keys are in same case and contains digits ',function () {
+    let kvParser = new StrictParser(["Adithi1"]);
+    let actual=kvParser.parse("Adithi1=object");
+    let expected=new Parsed();
+    expected['Adithi1']="object";
+    assert.deepEqual(actual,expected);
+  })
+
+  it("should parse when specified keys are in same case and contains special symbols ",function () {
+    let kvParser = new StrictParser(['Name_2']);
+    let expected = new Parsed();
+    let actual=kvParser.parse('Name_2=gollum');
+    expected['Name_2']="gollum";
+    assert.deepEqual(actual,expected);
+  })
 });

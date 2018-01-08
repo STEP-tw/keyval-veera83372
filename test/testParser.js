186cb809f773bca0f1c27c7b2fad9956cc4c04c6
veera83372 <veeravenky222@gmail.com>
2018-01-08 15:25:24 +0530

    Resolved : this reference in checkThrownError function

--- test/testParser.js
+++ test/testParser.js
@@ -1,287 +1,291 @@
-const src=function(filePath){return "../src/"+filePath};
-const errors=function(filePath){return "../src/errors/"+filePath};
+const src = function(filePath) {
+  return "../src/" + filePath
+};
+const errors = function(filePath) {
+  return "../src/errors/" + filePath
+};
 
 const chai = require('chai').assert;
-const Parser=require(src('index.js')).Parser;
+const Parser = require(src('index.js')).Parser;
 const Parsed = require(src('parsed.js'));
-const MissingValueError=require(errors('missingValueError.js'));
-const MissingEndQuoteError=require(errors('missingEndQuoteError.js'));
-const MissingKeyError=require(errors('missingKeyError.js'));
-const MissingAssignmentOperatorError=require(errors('missingAssignmentOperatorError.js'));
-const IncompleteKeyValuePairError=require(errors('incompleteKeyValuePairError.js'));
+const MissingValueError = require(errors('missingValueError.js'));
+const MissingEndQuoteError = require(errors('missingEndQuoteError.js'));
+const MissingKeyError = require(errors('missingKeyError.js'));
+const MissingAssignmentOperatorError = require(errors('missingAssignmentOperatorError.js'));
+const IncompleteKeyValuePairError = require(errors('incompleteKeyValuePairError.js'));
 
 var kvParser;
 var expected;
 
-describe("parse basic key values",function(){
-  beforeEach(function(){
-    kvParser=new Parser();
-    expected=new Parsed();
+describe("parse basic key values", function() {
+  beforeEach(function() {
+    kvParser = new Parser();
+    expected = new Parsed();
   });
 
-  it("parses an empty string",function(){
-    let actual=kvParser.parse("");
-    chai.equal(0,actual.length());
+  it("parses an empty string", function() {
+    let actual = kvParser.parse("");
+    chai.equal(0, actual.length());
   });
 
-  it("parse key=value",function(){
-    let actual=kvParser.parse("key=value");
-    chai.equal("value",actual.key);
-    chai.equal(1,actual.length());
+  it("parse key=value", function() {
+    let actual = kvParser.parse("key=value");
+    chai.equal("value", actual.key);
+    chai.equal(1, actual.length());
   });
 
-  it("parse when there are leading spaces before key",function(){
-    let actual=kvParser.parse(" key=value");
-    expected.key='value';
-    chai.deepEqual(expected,kvParser.parse(" key=value"));
+  it("parse when there are leading spaces before key", function() {
+    let actual = kvParser.parse(" key=value");
+    expected.key = 'value';
+    chai.deepEqual(expected, kvParser.parse(" key=value"));
   });
 
-  it("parse when there are spaces after key",function(){
-    expected.key='value';
-    chai.deepEqual(expected,kvParser.parse("key =value"));
+  it("parse when there are spaces after key", function() {
+    expected.key = 'value';
+    chai.deepEqual(expected, kvParser.parse("key =value"));
   });
 
-  it("parse when there are spaces before and after key",function(){
-    expected.key='value';
-    chai.deepEqual(expected,kvParser.parse(" key =value"));
+  it("parse when there are spaces before and after key", function() {
+    expected.key = 'value';
+    chai.deepEqual(expected, kvParser.parse(" key =value"));
   });
 
-  it("parse when there are spaces before value",function(){
-    expected.key='value';
-    chai.deepEqual(expected,kvParser.parse("key= value"));
+  it("parse when there are spaces before value", function() {
+    expected.key = 'value';
+    chai.deepEqual(expected, kvParser.parse("key= value"));
   });
 
-  it("parse when there are spaces after value",function(){
-    expected.key='value';
-    chai.deepEqual(expected,kvParser.parse("key=value "));
-  });
-});
-
-describe("parse digits and other special chars",function(){
-  beforeEach(function(){
-    kvParser=new Parser();
-    expected=new Parsed();
-  });
-
-  it("parse keys with a single digit",function(){
-    expected['1']='value';
-    chai.deepEqual(expected,kvParser.parse("1=value"));
-  });
-
-  it("parse keys with only multiple digits",function(){
-    expected['123']="value";
-    chai.deepEqual(expected,kvParser.parse("123=value"));
-  });
-
-  it("parse keys with leading 0s",function(){
-    expected['0123']="value";
-    chai.deepEqual(expected,kvParser.parse("0123=value"));
-  });
-
-  it("parse keys with underscores",function(){
-    expected['first_name']="value";
-    chai.deepEqual(expected,kvParser.parse("first_name=value"));
-  });
-
-  it("parse keys with a single underscore",function(){
-    expected['_']="value";
-    chai.deepEqual(expected,kvParser.parse("_=value"));
-  });
-
-  it("parse keys with multiple underscores",function(){
-    expected['__']="value";
-    chai.deepEqual(expected,kvParser.parse("__=value"));
-  });
-
-  it("parse keys with alphabets and digits(digits leading)",function(){
-    expected['0abc']="value";
-    chai.deepEqual(expected,kvParser.parse("0abc=value"));
-  });
-
-  it("parse keys with alphabets and digits(alphabets leading)",function(){
-    expected['a0bc']="value";
-    chai.deepEqual(expected,kvParser.parse("a0bc=value"));
+  it("parse when there are spaces after value", function() {
+    expected.key = 'value';
+    chai.deepEqual(expected, kvParser.parse("key=value "));
   });
 });
 
-describe("multiple keys",function(){
-  beforeEach(function(){
-    kvParser=new Parser();
-    expected=new Parsed();
+describe("parse digits and other special chars", function() {
+  beforeEach(function() {
+    kvParser = new Parser();
+    expected = new Parsed();
   });
 
-  it("parse more than one key",function(){
-    expected.key="value";
-    expected.anotherkey="anothervalue";
-    chai.deepEqual(expected,kvParser.parse("key=value anotherkey=anothervalue"));
+  it("parse keys with a single digit", function() {
+    expected['1'] = 'value';
+    chai.deepEqual(expected, kvParser.parse("1=value"));
   });
 
-  it("parse more than one key when keys have leading spaces",function(){
-    expected.key="value";
-    expected.anotherkey="anothervalue";
-    chai.deepEqual(expected,kvParser.parse("   key=value anotherkey=anothervalue"));
+  it("parse keys with only multiple digits", function() {
+    expected['123'] = "value";
+    chai.deepEqual(expected, kvParser.parse("123=value"));
   });
 
-  it("parse more than one key when keys have trailing spaces",function(){
-    expected.key="value";
-    expected.anotherkey="anothervalue";
-    chai.deepEqual(expected,kvParser.parse("key  =value anotherkey  =anothervalue"));
+  it("parse keys with leading 0s", function() {
+    expected['0123'] = "value";
+    chai.deepEqual(expected, kvParser.parse("0123=value"));
   });
 
-  it("parse more than one key when keys have leading and trailing spaces",function(){
-    expected.key="value";
-    expected.anotherkey="anothervalue";
-    chai.deepEqual(expected,kvParser.parse("  key  =value anotherkey  =anothervalue"));
-  });
-});
-
-describe("single values with quotes",function(){
-  beforeEach(function(){
-    kvParser=new Parser();
-    expected=new Parsed();
+  it("parse keys with underscores", function() {
+    expected['first_name'] = "value";
+    chai.deepEqual(expected, kvParser.parse("first_name=value"));
   });
 
-  it("parse a single value with quotes",function(){
-    expected.key="value";
-    chai.deepEqual(expected,kvParser.parse("key=\"value\""));
+  it("parse keys with a single underscore", function() {
+    expected['_'] = "value";
+    chai.deepEqual(expected, kvParser.parse("_=value"));
   });
 
-  it("parse a single quoted value that has spaces in it",function(){
-    expected.key="va lue";
-    chai.deepEqual(expected,kvParser.parse("key=\"va lue\""));
+  it("parse keys with multiple underscores", function() {
+    expected['__'] = "value";
+    chai.deepEqual(expected, kvParser.parse("__=value"));
   });
 
-  it("parse a single quoted value that has spaces in it and leading spaces",function(){
-    expected.key="va lue";
-    chai.deepEqual(expected,kvParser.parse("key=   \"va lue\""));
+  it("parse keys with alphabets and digits(digits leading)", function() {
+    expected['0abc'] = "value";
+    chai.deepEqual(expected, kvParser.parse("0abc=value"));
   });
 
-  it("parse a single quoted value that has spaces in it and trailing spaces",function(){
-    expected.key="va lue";
-    chai.deepEqual(expected,kvParser.parse("key=\"va lue\"   "));
+  it("parse keys with alphabets and digits(alphabets leading)", function() {
+    expected['a0bc'] = "value";
+    chai.deepEqual(expected, kvParser.parse("a0bc=value"));
   });
 });
 
-describe("multiple values with quotes",function(){
-  it("parse more than one value with quotes",function(){
-    expected.key="va lue";
-    expected.anotherkey="another value";
-    chai.deepEqual(expected,kvParser.parse("key=\"va lue\" anotherkey=\"another value\""));
+describe("multiple keys", function() {
+  beforeEach(function() {
+    kvParser = new Parser();
+    expected = new Parsed();
   });
 
-  it("parse more than one value with quotes with leading spaces",function(){
-    expected.key="va lue";
-    expected.anotherkey="another value";
-    chai.deepEqual(expected,kvParser.parse("key= \"va lue\" anotherkey= \"another value\""));
+  it("parse more than one key", function() {
+    expected.key = "value";
+    expected.anotherkey = "anothervalue";
+    chai.deepEqual(expected, kvParser.parse("key=value anotherkey=anothervalue"));
   });
 
-  it("parse more than one value with quotes when keys have trailing spaces",function(){
-    expected.key="va lue";
-    expected.anotherkey="another value";
-    chai.deepEqual(expected,kvParser.parse("key = \"va lue\" anotherkey = \"another value\""));
-  });
-});
-
-describe("mixed values with both quotes and without",function(){
-  it("parse simple values with and without quotes",function(){
-    expected.key="value";
-    expected.anotherkey="anothervalue";
-    chai.deepEqual(expected,kvParser.parse("key=value anotherkey=\"anothervalue\""));
+  it("parse more than one key when keys have leading spaces", function() {
+    expected.key = "value";
+    expected.anotherkey = "anothervalue";
+    chai.deepEqual(expected, kvParser.parse("   key=value anotherkey=anothervalue"));
   });
 
-  it("parse simple values with and without quotes and leading spaces on keys",function(){
-    expected.key="value";
-    expected.anotherkey="anothervalue";
-    chai.deepEqual(expected,kvParser.parse("   key=value anotherkey=\"anothervalue\""));
+  it("parse more than one key when keys have trailing spaces", function() {
+    expected.key = "value";
+    expected.anotherkey = "anothervalue";
+    chai.deepEqual(expected, kvParser.parse("key  =value anotherkey  =anothervalue"));
   });
 
-  it("parse simple values with and without quotes and trailing spaces on keys",function(){
-    expected.key="value";
-    expected.anotherkey="anothervalue";
-    chai.deepEqual(expected,kvParser.parse("key  =value anotherkey  =\"anothervalue\""));
-  });
-
-  it("parse simple values with and without quotes and leading and trailing spaces on keys",function(){
-    expected.key="value";
-    expected.anotherkey="anothervalue";
-    chai.deepEqual(expected,kvParser.parse("  key  =value anotherkey  = \"anothervalue\""));
-  });
-
-  it("parse simple values with and without quotes(quoted values first)",function(){
-    expected.key="value";
-    expected.anotherkey="anothervalue";
-    chai.deepEqual(expected,kvParser.parse("anotherkey=\"anothervalue\" key=value"));
+  it("parse more than one key when keys have leading and trailing spaces", function() {
+    expected.key = "value";
+    expected.anotherkey = "anothervalue";
+    chai.deepEqual(expected, kvParser.parse("  key  =value anotherkey  =anothervalue"));
   });
 });
 
-const errorChecker=function(key,pos,typeOfError) {
-    return function(err) {
-      if(err instanceof typeOfError && err.key==key && err.position==pos)
-        return true;
-      return false;
-    }
-}
+describe("single values with quotes", function() {
+  beforeEach(function() {
+    kvParser = new Parser();
+    expected = new Parsed();
+  });
 
-const checkThrownError = function (fn,args,checker) {
-  try{
-    fn(args);
-  }catch(err){
-    if(!errorChecker("key",3,MissingValueError));
-      throw err
+  it("parse a single value with quotes", function() {
+    expected.key = "value";
+    chai.deepEqual(expected, kvParser.parse("key=\"value\""));
+  });
+
+  it("parse a single quoted value that has spaces in it", function() {
+    expected.key = "va lue";
+    chai.deepEqual(expected, kvParser.parse("key=\"va lue\""));
+  });
+
+  it("parse a single quoted value that has spaces in it and leading spaces", function() {
+    expected.key = "va lue";
+    chai.deepEqual(expected, kvParser.parse("key=   \"va lue\""));
+  });
+
+  it("parse a single quoted value that has spaces in it and trailing spaces", function() {
+    expected.key = "va lue";
+    chai.deepEqual(expected, kvParser.parse("key=\"va lue\"   "));
+  });
+});
+
+describe("multiple values with quotes", function() {
+  it("parse more than one value with quotes", function() {
+    expected.key = "va lue";
+    expected.anotherkey = "another value";
+    chai.deepEqual(expected, kvParser.parse("key=\"va lue\" anotherkey=\"another value\""));
+  });
+
+  it("parse more than one value with quotes with leading spaces", function() {
+    expected.key = "va lue";
+    expected.anotherkey = "another value";
+    chai.deepEqual(expected, kvParser.parse("key= \"va lue\" anotherkey= \"another value\""));
+  });
+
+  it("parse more than one value with quotes when keys have trailing spaces", function() {
+    expected.key = "va lue";
+    expected.anotherkey = "another value";
+    chai.deepEqual(expected, kvParser.parse("key = \"va lue\" anotherkey = \"another value\""));
+  });
+});
+
+describe("mixed values with both quotes and without", function() {
+  it("parse simple values with and without quotes", function() {
+    expected.key = "value";
+    expected.anotherkey = "anothervalue";
+    chai.deepEqual(expected, kvParser.parse("key=value anotherkey=\"anothervalue\""));
+  });
+
+  it("parse simple values with and without quotes and leading spaces on keys", function() {
+    expected.key = "value";
+    expected.anotherkey = "anothervalue";
+    chai.deepEqual(expected, kvParser.parse("   key=value anotherkey=\"anothervalue\""));
+  });
+
+  it("parse simple values with and without quotes and trailing spaces on keys", function() {
+    expected.key = "value";
+    expected.anotherkey = "anothervalue";
+    chai.deepEqual(expected, kvParser.parse("key  =value anotherkey  =\"anothervalue\""));
+  });
+
+  it("parse simple values with and without quotes and leading and trailing spaces on keys", function() {
+    expected.key = "value";
+    expected.anotherkey = "anothervalue";
+    chai.deepEqual(expected, kvParser.parse("  key  =value anotherkey  = \"anothervalue\""));
+  });
+
+  it("parse simple values with and without quotes(quoted values first)", function() {
+    expected.key = "value";
+    expected.anotherkey = "anothervalue";
+    chai.deepEqual(expected, kvParser.parse("anotherkey=\"anothervalue\" key=value"));
+  });
+});
+
+const errorChecker = function(key, pos, typeOfError) {
+  return function(err) {
+    if (err instanceof typeOfError && err.key == key && err.position == pos)
+      return true;
+    return false;
   }
 }
 
-describe("error handling",function(){
-  beforeEach(function(){
-    kvParser=new Parser();
+const checkThrownError = function(fn, args, checker) {
+  try {
+    fn.call(kvParser, args);
+  } catch (err) {
+    if (checker(err));
+    throw err
+  }
+}
+
+describe("error handling", function() {
+  beforeEach(function() {
+    kvParser = new Parser();
   });
 
-  it("throws error on missing value when value is unquoted",function(){
+  it("throws error on missing value when value is unquoted", function() {
     chai.throws(
       () => {
-        checkThrownError(kvParser.parse,"key=",errorChecker("key",3,MissingValueError));
+        checkThrownError(kvParser.parse, "key=", errorChecker("key", 3, MissingValueError));
       });
   });
 
-  it("throws error on missing value when value is quoted",function(){
+  it("throws error on missing value when value is quoted", function() {
     chai.throws(
       () => {
-        checkThrownError(kvParser.parse,"key=\"value",
-        errorChecker("key",9,MissingEndQuoteError));
+        checkThrownError(kvParser.parse, "key=\"value",
+          errorChecker("key", 9, MissingEndQuoteError));
       })
   });
 
-  it("throws error on missing key",function(){
+  it("throws error on missing key", function() {
     chai.throws(
       () => {
-        checkThrownError(kvParser.parse,"=value",
-      errorChecker(undefined,0,MissingKeyError));
-    }
+        checkThrownError(kvParser.parse, "=value",
+          errorChecker(undefined, 0, MissingKeyError));
+      }
     )
   });
 
-  it("throws error on invalid key",function(){
+  it("throws error on invalid key", function() {
     chai.throws(
       () => {
-        checkThrownError(kvParser.parse,"'foo'=value",
-      errorChecker(undefined,0,MissingKeyError));
-    })
+        checkThrownError(kvParser.parse, "'foo'=value",
+          errorChecker(undefined, 0, MissingKeyError));
+      })
   });
 
-  it("throws error on missing assignment operator",function(){
+  it("throws error on missing assignment operator", function() {
     chai.throws(
       () => {
-        checkThrownError(kvParser.parse,"key value",
-      errorChecker(undefined,4,MissingAssignmentOperatorError));
-    })
+        checkThrownError(kvParser.parse, "key value",
+          errorChecker(undefined, 4, MissingAssignmentOperatorError));
+      })
   });
 
-  it("throws error on incomplete key value pair",function(){
+  it("throws error on incomplete key value pair", function() {
     chai.throws(
       () => {
-        checkThrownError(kvParser.parse,"key",
-      errorChecker(undefined,2,IncompleteKeyValuePairError));
-    }
+        checkThrownError(kvParser.parse, "key",
+          errorChecker(undefined, 2, IncompleteKeyValuePairError));
+      }
     )
   });
 
--- test/testStrictParser.js
+++ test/testStrictParser.js
@@ -1,110 +1,114 @@
-const src=function(filePath){return "../src/"+filePath};
-const errors=function(filePath){return "../src/errors/"+filePath};
+const src = function(filePath) {
+  return "../src/" + filePath
+};
+const errors = function(filePath) {
+  return "../src/errors/" + filePath
+};
 
 
 const chai = require('chai').assert;
 const Parsed = require(src('parsed.js'));
-const StrictParser=require(src('index.js')).StrictParser;
-const InvalidKeyError=require(errors('invalidKeyError.js'));
+const StrictParser = require(src('index.js')).StrictParser;
+const InvalidKeyError = require(errors('invalidKeyError.js'));
 
-var invalidKeyErrorChecker=function(key,pos) {
+var invalidKeyErrorChecker = function(key, pos) {
   return function(err) {
-    if(err instanceof InvalidKeyError && err.invalidKey==key && err.position==pos)
+    if (err instanceof InvalidKeyError && err.invalidKey == key && err.position == pos)
       return true;
     return false;
   }
 }
 
-const checkThrownError = function (fn,args,checker) {
-  try{
-    fn(args);
-  }catch(err){
-    if(!errorChecker("key",3,MissingValueError));
-      throw err
+const checkThrownError = function(fn, args, checker, kvParser) {
+  try {
+    fn.call(kvParser, args);
+  } catch (err) {
+    if (checker(err));
+    throw err
   }
 }
 
-describe("strict parser",function(){
-  it("should only parse keys that are specified for a single key",function(){
-    let kvParser=new StrictParser(["name"]);
+describe("strict parser", function() {
+  it("should only parse keys that are specified for a single key", function() {
+    let kvParser = new StrictParser(["name"]);
     chai.throws(
       () => {
-        checkThrownError(kvParser.parse,"age=23",
-        invalidKeyErrorChecker("age",5));
+        checkThrownError(kvParser.parse, "age=23",
+          invalidKeyErrorChecker("age", 5), kvParser);
       })
   });
 
-  it("should only parse keys that are specified for multiple keys",function(){
-    let kvParser=new StrictParser(["name","age"]);
-    let actual=kvParser.parse("name=john age=23");
-    let expected=new Parsed()
-    expected.name="john";
-    expected.age="23";
-    chai.deepEqual(expected,actual);
+  it("should only parse keys that are specified for multiple keys", function() {
+    let kvParser = new StrictParser(["name", "age"]);
+    let actual = kvParser.parse("name=john age=23");
+    let expected = new Parsed()
+    expected.name = "john";
+    expected.age = "23";
+    chai.deepEqual(expected, actual);
     chai.throws(
       () => {
-        checkThrownError(kvParser.parse,"color=blue",
-        invalidKeyErrorChecker("color",9));
+        checkThrownError(kvParser.parse, "color=blue",
+          invalidKeyErrorChecker("color", 9), kvParser);
       }
-      )
+    )
   });
 
-  it("should throw an error when one of the keys is not valid",function(){
+  it("should throw an error when one of the keys is not valid", function() {
     chai.throws(
       () => {
-        let kvParser=new StrictParser(["name","age"]);
-        checkThrownError(kvParser.parse,"name=john color=blue age=23",
-        invalidKeyErrorChecker("color",20));
+        let kvParser = new StrictParser(["name", "age"]);
+        checkThrownError(kvParser.parse, "name=john color=blue age=23",
+          invalidKeyErrorChecker("color", 20), kvParser);
       }
-      )
+    )
   });
 
-  it("should throw an error on invalid key when there are spaces between keys and assignment operators",function(){
+  it("should throw an error on invalid key when there are spaces between keys and assignment operators", function() {
     chai.throws(
       () => {
-        let kvParser=new StrictParser(["name","age"]);
-        checkThrownError(kvParser.parse,"color   = blue",
-        invalidKeyErrorChecker("color",13));
+        let kvParser = new StrictParser(["name", "age"]);
+        checkThrownError(kvParser.parse, "color   = blue",
+          invalidKeyErrorChecker("color", 13), kvParser);
       })
   });
 
-  it("should throw an error on invalid key when there are quotes on values",function(){
+  it("should throw an error on invalid key when there are quotes on values", function() {
     chai.throws(
       () => {
-        let kvParser=new StrictParser(["name","age"]);
-        checkThrownError(kvParser.parse,"color   = \"blue\"",
-        invalidKeyErrorChecker("color",15));
+        let kvParser = new StrictParser(["name", "age"]);
+        checkThrownError(kvParser.parse, "color   = \"blue\"",
+          invalidKeyErrorChecker("color", 15), kvParser);
       }
-      )
+    )
   });
 
-  it("should throw an error on invalid key when there are cases of both quotes and no quotes",function(){
+  it("should throw an error on invalid key when there are cases of both quotes and no quotes", function() {
     chai.throws(
       () => {
-        let kvParser=new StrictParser(["name","age"]);
-        checkThrownError(kvParser.parse,"name = john color   = \"light blue\"",
-        invalidKeyErrorChecker("color",33))
+        let kvParser = new StrictParser(["name", "age"]);
+        checkThrownError(kvParser.parse, "name = john color   = \"light blue\"",
+          invalidKeyErrorChecker("color", 33), kvParser)
       }
-      )
+    )
   });
 
-  it("should throw an error when no valid keys are specified",function(){
+  it("should throw an error when no valid keys are specified", function() {
     chai.throws(
       () => {
-        let kvParser=new StrictParser([]);
-        checkThrownError(kvParser.parse,"name=john",
-        invalidKeyErrorChecker("name",8))
+        let kvParser = new StrictParser([]);
+        checkThrownError(kvParser.parse, "name=john",
+          invalidKeyErrorChecker("name", 8), kvParser)
       })
 
   });
 
-  it("should throw an error when no array is passed",function(){
+  it("should throw an error when no array is passed", function() {
     chai.throws(
       () => {
-        let kvParser=new StrictParser();
-      checkThrownError(kvParser.parse,"name=john",
-      invalidKeyErrorChecker("name",8));
-    })
+        let kvParser = new StrictParser();
+        checkThrownError(kvParser.parse, "name=john",
+          invalidKeyErrorChecker("name", 8), kvParser);
+      })
 
   });
 
